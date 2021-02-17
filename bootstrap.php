190135f5032518d7a3e11c12d5eaa7e18c160170
Tu Shi <st19901114@live.cn>
2021-02-17 08:50:16 -0500

    Add files via upload

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+
+require __DIR__.'/vendor/autoload.php';
+
+
+$configuration = array(
+    'db_dsn' =>'mysql:host=localhost;dbname=oo_battles',
+    'db_user' => 'root',
+    'db_pass' => '',
+);
